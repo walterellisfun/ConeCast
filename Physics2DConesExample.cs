9960e70643f5d2b54b3076a65f17d164a88550c9
Daniel Ashby <ashbygeek@gmail.com>
2019-08-02 22:58:55 -0400

    Added Physics2D equivalent and fleshed out the concept a little.

---
+++ Physics2DConesExample.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class ConeCast2DExample : MonoBehaviour {
+
+    public float radius;
+    public float depth;
+    public float angle;
+
+    private Physics physics;
+
+	void FixedUpdate ()
+    {
+        RaycastHit2D[] coneHits = Physics2DCones.ConeCastAll(transform.position, transform.forward, angle, depth);
+
+        for (int i = 0; i < coneHits.Length; i++)
+        {
+            //do something with collider information
+            coneHits[i].collider.gameObject.GetComponent<Renderer>().material.color = new Color(0, 0, 1f);
+        }
+	}
+}
+
+
+public class ConeCast2DExample2 : MonoBehaviour
+{
+    public float radius;
+    public float depth;
+    public float angle;
+
+    private RayCastHit2D[] hits = new RayCastHit2D[100];
+
+    void FixedUpdate()
+    {
+
+        int numResults = Physics2DCones.ConeCastNonAlloc(transform.position, radius, transform.forward, hits, depth);
+
+        for (int i = 0; i < numResults; i++)
+        {
+            //do something with collider information
+            hits[i].collider.gameObject.GetComponent<Renderer>().material.color = new Color(0, 0, 1f);
+        }
+    }
+}
